6ec3b7a3c0c30e36f67967f4bb3289dd6485cc0f
Robin Drexler <drexler.robin@gmail.com>
2022-01-23 00:10:08 -0500

    first crappy version

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,1 +1,14 @@
-console.log(`'Allo 'Allo! Content script`)
+let circuitBreaker = 0;
+
+// MutationObserver somehow didn't work 🤷
+const timer = setInterval(() => {
+  const path = document.querySelector("#hex-hw-shapeclip-clipconfig");
+  if (path) {
+    path.innerHTML = '<circle cx="50%" cy="50%" r="85"></circle>';
+  }
+  if (path || circuitBreaker > 1000) {
+    clearInterval(timer);
+  }
+
+  circuitBreaker++;
+}, 50);
